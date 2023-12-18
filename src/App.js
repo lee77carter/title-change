9f9968f7287577229415bd34df64732eddb64f60
lee_c <gitleelee@gmail.com>
2023-12-17 17:53:12 -0800

    updates to change browser title.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import "./styles.css";
 import { useState, useEffect } from "react";
 
 const App = () => {
+  const [documentTitle, setDocumentTitle] = useState("");
+
+  useEffect(() => {
+    document.title = documentTitle;
+  }, [documentTitle]);
+
+  const handleTitleChange = (e) => {
+    setDocumentTitle(e.target.value);
+  };
+
   return (
     <div className="App">
       <label htmlFor="title-input">
         Type in the input and look at the browser tab!
       </label>
-      <input id="title-input" type="text" maxLength={25} />
+      <input 
+      id="title-input" 
+      type="text" 
+      value={documentTitle}
+      onChange={(handleTitleChange)}
+      maxLength={25} 
+      />
     </div>
   );
 };
